4f358914704ce33c7f68fd4c2b1da977b2add0aa
NguyenQuocDuy <bachhieuvannn@gmail.com>
2023-09-15 21:28:07 +0700

    create project,...

--- src/main/java/vn/edu/iuh/fit/labweek0120102201/controllers/ControllerServlet.java
+++ src/main/java/vn/edu/iuh/fit/labweek0120102201/controllers/ControllerServlet.java
@@ -1,18 +1,18 @@
 package vn.edu.iuh.fit.labweek0120102201.controllers;
 
-import jakarta.servlet.ServletException;
+
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import javax.swing.*;
+
 import java.io.IOException;
 
 @WebServlet(urlPatterns = {"/ControllerServlet"})
 public class ControllerServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws  IOException {
         resp.getWriter().println("hello");
 
 
@@ -20,7 +20,7 @@ public class ControllerServlet extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.getWriter().println("hello");
     }
 }
